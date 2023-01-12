b4fe5b88d5ef4ddb2d9c16dae8d172dc8070fcc5
William Symes <wwsorcas@gmail.com>
2023-01-12 14:43:17 -0800

    fixed trcg

--- pyrvl/vcalg.py
+++ pyrvl/vcalg.py
@@ -1,7 +1,12 @@
+import gc
+import os
 import vcl
 import math
 from vcl import Vector
 from vcl import transp
+
+# single steps are separated from loops as separate functions in order
+# to force garbage-collection.
 
 ###################### CONJUGATE GRADIENT ITERATION ######################
 
@@ -16,36 +21,97 @@ from vcl import transp
 #  kmax = max iterations for termination
 #  eps = relative residual decrease for termination
 #  rho = relative normal residual decrease for termination
-#  e = residual on return
-#  r = normal residual on return
+#  e = residual on return (optional)
+#  r = normal residual on return (optional)
 #  verbose = verbosity level
 #    0 - no print output
 #    1 - print number of its performed, residual and normal residual
 #    2 - (or more) print it, res, nres at every it
 
-def conjgrad(x, b, A, kmax, eps, rho, e, r, verbose=0):
+def cgstep(x, A, k, p, r, e, rnorm, enorm, gamma, verbose):
+#    os.system('echo top; ls /var/tmp')
+    try:
+        # print('k='+str(k))
+
+        # print('#1. $q = Ap$')
+        q = A*p
+            
+        # print('#2. $s = A^Tq$')
+        s = transp(A)*q
+            
+        #os.system('echo q s; ls /var/tmp')
+            
+        # print('#3. $\alpha = gamma / \langle p, s\rangle$')
+        alpha = gamma/p.dot(s)
+
+        # print('#4. $x \leftarrow x+\alpha p$')
+        x.linComb(alpha,p)
+
+        # print('#5. $e \leftarrow e-\alpha q$')
+        e.linComb(-alpha,q)
+        enorm = e.norm()
+        
+        # print('#6. $r \leftarrow r-\alpha s$')
+        r.linComb(-alpha,s)
+        
+        # print('#7. $\delta = \langle r, r \rangle$')  
+        delta = r.dot(r)
+        rnorm = math.sqrt(delta)
+        
+        # print('#8. $\beta = \delta / \gamma$')
+        beta = delta/gamma
+        
+        # print('#9. $p \leftarrow r + \beta p$')
+        p.linComb(1.0,r,beta)
+        
+        # print('#10. $\gamma \leftarrow delta$')
+        gamma=delta
+        
+        # print('#11. $k \leftarrow k+1$')
+        k=k+1
+        
+        # print('#12. print $k$ (iteration) $\|e\|$ (residual norm),
+        # $\|r\|$ (normal residual norm)')
+        if verbose > 1:
+            print('%3d  %10.4e  %10.4e' % (k, enorm, rnorm))
+
+        # without the following code, the vector destructor is not called
+        # until the program exits
+        # reduce ref count for q, s to 1, which is unreachable - the
+        # reference created by invoking the constructor 
+        del q
+        del s
+        # force garbage collection
+        n=gc.collect()
+
+        return [k, enorm, rnorm, gamma]
+
+    except Exception as ex:
+        print(ex)
+        raise Exception('called from cgstep')
+                
+def conjgrad(x, b, A, kmax, eps, rho, verbose=0, e=None, r=None):
 
     try:
 
-        # p = Vector(A.getDomain())
-        # s = Vector(A.getDomain())
-        # q = Vector(A.getRange()) 
-        
         #Initialize:
         # print('#1. $x = 0$')
         x.scale(0.0)
         
         # print('#2. $e = b$')
-        e.copy(b)
+        if e is None:
+            e = b.dup()
+        else:
+            e.copy(b)
         
         # print('#3. $r = A^Tb$')
-        # A.applyAdj(b,r)
-        # r.copy(transp(A)*b)
         p = transp(A)*b
         
         # print('#4. $p = r$')
-        #p.copy(r)
-        r.copy(p)
+        if r is None:
+            r = p.dup()
+        else:
+            r.copy(p)
         
         # print('#7. $\gamma_0 = \langle r, r \rangle$')
         gamma0 = r.dot(r)
@@ -64,60 +130,11 @@ def conjgrad(x, b, A, kmax, eps, rho, e, r, verbose=0):
             print('  k       |e|       |r|=')
             print('%3d  %10.4e  %10.4e' % (k, enorm, rnorm))
     
-
         #Repeat while $k<k_{\rm max}$, $\|e\|>\epsilon \|d\|$:
         while k<kmax and enorm>eps*enorm0 and rnorm>rho*rnorm0:
-            # print('k='+str(k))
 
-            # print('#1. $q = Ap$')
-            # A.applyFwd(p,q)
-            # q.copy(A*p)
-            q = A*p
+            [k, enorm, rnorm, gamma] = cgstep(x, A, k, p, r, e, rnorm, enorm, gamma, verbose)
             
-            # print('#2. $s = A^Tq$')
-            # A.applyAdj(q,s)
-            # s.copy(transp(A)*q)
-            s = transp(A)*q
-            
-            # print('#3. $\alpha = gamma / \langle q, q\rangle$')
-            # write it this way, as quotient of norms, to avoid
-            # possible precision issues at sqrt(macheps) level
-            # alpha = math.sqrt(gamma)/q.norm()
-            # alpha = alpha*alpha
-            # this is equivalent:
-            alpha = gamma/p.dot(s)
-
-            # print('#4. $x \leftarrow x+\alpha p$')
-            x.linComb(alpha,p)
-
-            # print('#5. $e \leftarrow e-\alpha q$')
-            e.linComb(-alpha,q)
-            enorm = e.norm()
-
-            # print('#6. $r \leftarrow r-\alpha s$')
-            r.linComb(-alpha,s)
-
-            # print('#7. $\delta = \langle r, r \rangle$')  
-            delta = r.dot(r)
-            rnorm = math.sqrt(delta)
-
-            # print('#8. $\beta = \delta / \gamma$')
-            beta = delta/gamma
-
-            # print('#9. $p \leftarrow r + \beta p$')
-            p.linComb(1.0,r,beta)
-
-            # print('#10. $\gamma \leftarrow delta$')
-            gamma=delta
-
-            # print('#11. $k \leftarrow k+1$')
-            k=k+1
-
-            # print('#12. print $k$ (iteration) $\|e\|$ (residual norm),
-            # $\|r\|$ (normal residual norm)')
-            if verbose > 1:
-                print('%3d  %10.4e  %10.4e' % (k, enorm, rnorm))
-                
         if verbose > 0:
             print('----------------------------------------------------')
             print('  k       |e|     |e|/|e0|      |r|        |r|/r0|')
@@ -129,63 +146,8 @@ def conjgrad(x, b, A, kmax, eps, rho, e, r, verbose=0):
         
 
 ########### TRUST-RADIUS MODIFIED CONJUGATE GRADIENT ITERATION ##########
+def trcgstep(x, A, k, p, r, rnorm, rnorm0, rho, gamma, Delta, cgend, verbose):
 
-# residual and normal residual vectors e and r are internal
-# "normal" termination when normal residual falls below tolerance
-# so no need to test residual
-
-# args:
-#  x = estimated solution on return
-#  b = data
-#  A = operator
-#  kmax = max iterations for termination
-#  rho = relative normal residual decrease for termination
-#  Delta = trust radius
-#  verbose = verbosity level
-#    0 - no print output
-#    1 - print number of its performed, residual and normal residual
-#    2 - (or more) print it, res, nres at every it
-
-def trconjgrad(x, b, A, kmax, rho, Delta, verbose=0):
-
-    try:
-
-        
-        #Initialize:
-        # print('#1. $x = 0$')
-        x.scale(0.0)
-        
-        # print('#3. $r = A^Tb$')
-        # A.applyAdj(b,r)
-        # r.copy(transp(A)*b)
-        r = transp(A)*b
-        # r = b.dup()
-        
-        # print('#4. $p = r$')
-        #p.copy(r)
-        p = vcl.Vector(A.getDomain())
-        p.copy(r)
-        # p = r.dup()
-
-        # print('#5. $\gamma_0 = \langle r, r \rangle$')
-        gamma0 = r.dot(r)
-        
-        # print('#6. $\gamma = \gamma_0$')
-        gamma=gamma0
-        
-        # print('#7. $k=0$')
-        k=0
-        rnorm0=r.norm()
-        rnorm=rnorm0
-
-        xnorm = x.norm()
-    
-        if verbose > 1:
-            print('  k       |r|')
-            print('%3d  %10.4e' % (k, rnorm))    
-
-        #Repeat while $k<k_{\rm max}$, rnorm > rho*rnorm0, |x|<Delta
-        while k<kmax and rnorm>rho*rnorm0 and xnorm<Delta:
             # print('k='+str(k))
 
             # print('#1. $q = Ap$')
@@ -206,6 +168,7 @@ def trconjgrad(x, b, A, kmax, rho, Delta, verbose=0):
             if xnorm > Delta:
 
                 x.scale(Delta/xnorm)
+                cgend = True
                 if verbose > 0:
                     print('trust radius exceeded - return scaled ' + \
                               'soln of length ' + str(Delta))
@@ -237,11 +200,75 @@ def trconjgrad(x, b, A, kmax, rho, Delta, verbose=0):
             # $\|r\|$ (normal residual norm)')
                 if verbose > 1:
                     print('%3d  %10.4e' % (k, rnorm))
-                
+
+            del q
+            del s
+            # force garbage collection
+            n=gc.collect()
+
+            return [x, k, p, r, rnorm, gamma, cgend]
+    
+# residual and normal residual vectors e and r are internal
+# "normal" termination when normal residual falls below tolerance
+# so no need to test residual
+
+# args:
+#  x = estimated solution on return
+#  b = data
+#  A = operator
+#  kmax = max iterations for termination
+#  rho = relative normal residual decrease for termination
+#  Delta = trust radius
+#  verbose = verbosity level
+#    0 - no print output
+#    1 - print number of its performed, residual and normal residual
+#    2 - (or more) print it, res, nres at every it
+
+def trconjgrad(x, b, A, kmax, rho, Delta, verbose=0):
+
+    try:
+
+        #Initialize:
+        # print('#1. $x = 0$')
+        x.scale(0.0)
+        
+        # print('#3. $r = A^Tb$')
+        r = transp(A)*b
+        
+        # print('#4. $p = r$')
+        p = r.dup()
+
+        # print('#5. $\gamma_0 = \langle r, r \rangle$')
+        gamma0 = r.dot(r)
+        
+        # print('#6. $\gamma = \gamma_0$')
+        gamma=gamma0
+        
+        # print('#7. $k=0$')
+        k=0
+        rnorm0=r.norm()
+        rnorm=rnorm0
+
+        xnorm = x.norm()
+    
+        if verbose > 1:
+            print('  k       |r|')
+            print('%3d  %10.4e' % (k, rnorm))    
+
+        # flag to indicate trust region truncation, indicating
+        # return to gn loop
+        cgend = False
+        
+        #Repeat while $k<k_{\rm max}$, rnorm > rho*rnorm0, |x|<Delta
+        while k<kmax and rnorm>rho*rnorm0 and not cgend:
+            
+            [x, k, p, r, rnorm, gamma, cgend] = trcgstep(x, A, k, p, r, rnorm, rnorm0, rho, gamma, Delta, cgend, verbose)
+            
         if verbose > 0:
             print('----------------------------------------------------')
             print('  k       |r|        |r|/r0|')
             print('%3d  %10.4e  %10.4e' % (k, rnorm, rnorm/rnorm0))
+
 
     except Exception as ex:
         print(ex)
@@ -286,7 +313,7 @@ def trgn(x, b, F, imax, eps, kmax, rho, Delta, mured=0.5, muinc=1.8, \
         gnorm0 = gnorm
 
         # storage for step
-        s=vcl.Vector(F.getDomain())
+        s=Vector(F.getDomain())
 
         # storage for trial update, residual
         #xp = vcl.Vector(F.getDomain())
