0756cae3cedf0155ec555a4c4883a5bb293c68bb
William Symes <wwsorcas@gmail.com>
2023-01-13 16:32:00 -0800

    portable scons

---
+++ scons/scons-local-4.3.1/SCons/__init__.py
@@ -0,0 +1,9 @@
+__version__="4.3.1"
+__copyright__="Copyright (c) 2001 - 2022 The SCons Foundation"
+__developer__="bdbaddog"
+__date__="Tue, 26 Jul 2022 16:33:08 -0700"
+__buildsys__="M1Dog2021"
+__revision__="1c635444d934111aa32077bccec25c1d5f76363e"
+__build__="1c635444d934111aa32077bccec25c1d5f76363e"
+# make sure compatibility is always in place
+import SCons.compat # noqa
