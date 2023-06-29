48d3bb8c61bc661f7c0c9fc707ef834046016904
William Symes <wwsorcas@gmail.com>
2023-06-29 11:51:40 -0700

    added docstrings

--- pyrvl/awi.py
+++ pyrvl/awi.py
@@ -9,6 +9,13 @@ import tempfile
 TRIP = os.getenv('TRIP')
 
 def setrms(u, urms):
+    '''
+    Calculate and store L2 norms of each trace in a SEGY file.
+    Parameters:
+    u (string): filename of input data traces
+    urms (string): filename output data traces of length 1, 
+    each containing L2 norm of corresponding trace in input file.
+    '''
     try:
         if not isinstance(TRIP,str):
             raise Exception('string TRIP not defined')
@@ -26,6 +33,22 @@ def setrms(u, urms):
         raise Exception('called from setrms')
 
 def compawipen(u, p, u0rms, precond, alpha):
+    '''
+    Compute action of AWI penalty operator on a set of traces. Multiplies 
+    trace samples by time and penalty weight, optionally scales by 
+    reciprocal of trace-dependent scale factor, intended to be L2 norms 
+    of traces for alpha=0 AWI source.
+    Parameters:
+    u (string): filename of input SEGY traces
+    p (string): filename of output SEGY traces, same geometry as
+    input traces (u)
+    u0rms (string): filename of auxiliary length-1 traces containing
+    scale factors for preconditioning. Same number of traces as input
+    (u) and output (p)
+    precond (int): preconditioning flag. 0 = no scaling, else scaling
+    applied
+    alpha (float): penalty weight
+    '''
     try:
         #print('compawipen: u=' + u + ' p=' + p + ' u0rms=' + u0rms)
         if not isinstance(TRIP,str):
@@ -60,7 +83,7 @@ class awipen(vcl.LinearOperator):
         verbose (int): CG verbosity flag
     '''
 
-    def __init__(self, dom, d, p, precond, alpha, kmax, eps, rho, verbose):
+    def __init__(self, dom, m, w, d, p, precond, alpha, kmax, eps, rho, verbose):
         try:
             self.dom = dom
             self.d = d
@@ -71,24 +94,28 @@ class awipen(vcl.LinearOperator):
             self.eps = eps
             self.rho = rho
             self.verbose = verbose
-            datapath = os.getenv('DATAPATH')
-            if not os.path.exists(datapath):
-                raise Exception('Error: datapath = ' + datapath + ' not valid path')
-            temp = tempfile.NamedTemporaryFile(delete=False,dir=datapath,suffix='.su')
-            temp.close()
-            self.u0rms = temp.name
-            print('in awipen constructor: u0rms=' + self.u0rms)
-            os.system('touch ' + self.u0rms)
+            self.u0rms = None
+            ### preconditioning setup
+            if precond != 0:
+                datapath = os.getenv('DATAPATH')
+                if not os.path.exists(datapath):
+                    raise Exception('Error: datapath = ' + datapath + ' not valid path')
+                temp = tempfile.NamedTemporaryFile(delete=False,dir=datapath,suffix='.su')
+                temp.close()
+                self.u0rms = temp.name
+                print('in awipen constructor: u0rms=' + self.u0rms)
+                os.system('touch ' + self.u0rms)
             
-            if d.space != p.space:
-                raise Exception('observed, predicted data spaces differ')
-            Sm0 = segyvc.ConvolutionOperator(dom=self.dom, rng=self.d.space, \
-                                green=self.p.data)
-            print('in awipen constructor: compute u0')
-            u0=vcl.Vector(self.dom)
-            vcalg.conjgrad(x=u0, b=self.d, A=Sm0, kmax=self.kmax, \
-               eps=self.eps, rho=self.rho, verbose=self.verbose)
-            setrms(u0.data,self.u0rms)
+                if d.space != p.space:
+                    raise Exception('observed, predicted data spaces differ')
+                Sm0 = segyvc.ConvolutionOperator(dom=self.dom,
+                                            rng=self.d.space,
+                                            green=self.p.data)
+                print('in awipen constructor: compute u0')
+                u0=vcl.Vector(self.dom)
+                vcalg.conjgrad(x=u0, b=self.d, A=Sm0, kmax=self.kmax, \
+                eps=self.eps, rho=self.rho, verbose=self.verbose)
+                setrms(u0.data,self.u0rms)
 
             self._unlink = os.unlink
             
@@ -97,7 +124,8 @@ class awipen(vcl.LinearOperator):
             raise Exception('called from awipen constructor')
 
     def __del__(self):
-        self._unlink(self.u0rms)
+        if self.u0rms is not None:
+            self._unlink(self.u0rms)
 
     def getDomain(self):
         return self.dom
@@ -138,86 +166,4 @@ class awipen(vcl.LinearOperator):
         print('    relative normal residual tolerance = ' + str(self.rho))
         print('    CG verbosity flag = ' + str(self.verbose))
         print('    filename for filter trace rms = ' + self.u0rms)
-
-class awiop(vcl.LinearOperator):
-
-    def __init__(self, dom, d, p, precond, alpha, kmax, eps, rho, verbose):
-        try:
-            self.dom = dom
-            self.d = d
-            self.p = p
-            self.precond = precond
-            self.alpha = alpha
-            self.kmax = kmax
-            self.eps = eps
-            self.rho = rho
-            self.verbose = verbose
-
-            self.rng = vcl.ProductSpace([self.d.space, self.dom])
-
-            self.Sm0 = segyvc.ConvolutionOperator(self.dom, self.d.space, self.p.data)
-            self.alphaT = awipen(dom, d, p, precond, alpha, kmax, eps, rho, verbose)
-
-        except Exception as ex:
-            print(ex)
-            raise Exception('called from awiop constructor')
-
-    def getDomain(self):
-        return self.dom
-
-    def getRange(self):
-        return self.rng
-
-    def applyFwd(self,dx,dy):
-        try:
-            #dy[0]=self.Sm0*dx
-            #dy[1]=self.alphaT*dx
-            self.Sm0.applyFwd(dx,dy[0])
-            self.alphaT.applyFwd(dx,dy[1])
-        except Exception as ex:
-            print(ex)
-            raise Exception('called from awiop.applyFwd')
-        else:
-            return dy
-        
-    def applyAdj(self,dx,dy):
-        try:
-#            print('AWI Op: applyAdj')
-#            print('transp comp 0')
-#            print('|dx[0]|=' + str(dx[0].norm()))
-            # cy = vcl.transp(self.Sm0)*dx[0]
-            cy = vcl.Vector(self.getDomain())
-            self.Sm0.applyAdj(dx[0],cy)
-#            print('|cy|=' + str(cy.norm()))
-#            print('transp comp 1')
-#            print('|dx[1]|=' + str(dx[1].norm()))
-#            #dy = vcl.transp(self.alphaT)*dx[1]
-            self.alphaT.applyAdj(dx[1],dy)
-#            print('|dy|=' + str(dy.norm()))
-#            print('lincomb')
-            dy.linComb(1.0,cy)
-#            print('|dy|=' + str(dy.norm()))
-#            dy.myNameIs()
-#            print('exit AWI Op: applyAdj')
-        except Exception as ex:
-            print(ex)
-            raise Exception('called from awiop.applyAdj')
-        else:
-            return dy
-
-    def myNameIs(self):
-        print('AWI operator')
-        print('domain = ')
-        self.dom.myNameIs()
-        print('range = data space OPLUS domain')
-        print('observed data:')
-        self.d.myNameIs()
-        print('predicted data:')
-        self.p.myNameIs()
-        print('preconditioning flag = ' + str(self.precond))
-        print('penalty weight = ' + str(self.alpha))
-        print('max CG iterations = ' + str(self.kmax))
-        print('relative residual tolerance = ' + str(self.eps))
-        print('relative normal residual tolerance = ' + str(self.rho))
-        print('CG verbosity flag = ' + str(self.verbose))
 
