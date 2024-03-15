f5c8e17b351efaa3307037f33863e34f49febd6c
William Symes <wwsorcas@gmail.com>
2024-03-15 09:06:48 -0700

    SPD smoother

--- repro/papers/awinum/mswi/msmswi.py
+++ repro/papers/awinum/mswi/msmswi.py
@@ -11,45 +11,6 @@ import segyvc
 import rsfvc
 import asg
 import awi
-import tmp
-
-class smoother(vcl.LinearOperator):
-
-    def __init__(self, dom, rect1=10, rect2=10, repeat=2):
-        RSFROOT = os.getenv('RSFROOT')
-        smoother = os.path.join(RSFROOT,'bin/sfsmooth')
-        self.dom = dom
-        if not isinstance(self.dom,rsfvc.Space):
-            raise Exception('grid smoother: provided domain not rsfvc.Space')
-        self.cmd = smoother + ' rect1=' + str(rect1) + ' rect2=' + str(rect2) + ' repeat=' +str(repeat)
-        
-    def getDomain(self):
-        return self.dom
-
-    def getRange(self):
-        return self.dom
-
-    def applyFwd(self,x,y):
-        try:
-            ret = os.system(self.cmd + ' < ' + x.data + ' > ' + y.data)
-            if ret != 0:
-                raise Exception('command failed: ' + self.cmd)
-        except Exception as ex:
-            print(ex)
-            raise Exception('called from smoother.applyFwd')
-
-    def applyAdj(self,x,y):
-        try:
-            ret = os.system(self.cmd + ' < ' + x.data + ' > ' + y.data)
-            if ret != 0:
-                raise Exception('command failed: ' + self.cmd)
-        except Exception as ex:
-            print(ex)
-            raise Exception('called from smoother.applyAdj')
-
-    def myNameIs():
-        print('2D Grid Smoother using sfsmooth')
-        print('rect1=10 rect2=10 repeat=2')
 
 # arguments:
 # output bulk mod, output filter,
@@ -64,12 +25,14 @@ def mswiopt(bulkmodout, filterout,
                 bulkmodin, datain, filterin, buoyancy, source, 
                 order=2, sampord=1, nsnaps=20,\
                 cfl=0.5, cmin=1.0, cmax=3.0,dmin=0.8, dmax=3.0,\
-                nl1=250, nr1=250, nl2=250, nr2=250, pmlampl=1.0,
-                boundstest=True, noisy=False,
-                rect1=10, rect2=10, repeat=2,
-                alpha=0.0001, sigma=0.00001, rho=0.0025, kmax=1000, verbose=0,
-                descmax=12, desceps=0.01, descverbose=1, descout=None,
-                lsmax=10, mured=0.5, muinc=1.8, gammared=0.1, gammainc=0.9, lsverbose=1
+                nl1=250, nr1=250, nl2=250, nr2=250, pmlampl=1.0,\
+                boundstest=True, noisy=False, partask=0,\
+                rect1=10, rect2=10, repeat=2,\
+                alpha=0.0001, sigma=0.00001, rho=0.0025, kmax=1000, verbose=0,\
+                etar=None, ratminus=None, ratplus=None,\
+                descmax=12, desceps=0.01, descverbose=1, descout=None,\
+                lsmax=10, mured=0.5, muinc=1.8, gammared=0.1, gammainc=0.9,\
+                lsverbose=1
                 ):
     try:
         
@@ -82,19 +45,22 @@ def mswiopt(bulkmodout, filterout,
         m0 = vcl.Vector(bulksp,bulkmodin)
         d = vcl.Vector(datasp,datain)
 
-        Winv = smoother(bulksp,rect1,rect2,repeat)
+        Winv = rsfvc.smoother(bulksp,rect1,rect2,repeat)
         
         F = asg.fsbop(bulksp, datasp, buoyancy, source, \
                     order, sampord, nsnaps, cfl, cmin, cmax, dmin, dmax,\
-                    nl1, nr1, nl2, nr2, pmlampl, boundstest, noisy \
+                    nl1, nr1, nl2, nr2, pmlampl, boundstest, noisy, partask \
                     )
         
-        mswiargs = dict(dom=usp, sim=F, data=d, alpha=alpha, sigma=sigma, kmax=kmax, rho=rho, verbose=verbose)
+        mswiargs = dict(dom=usp, sim=F, data=d, alpha=alpha, sigma=sigma, \
+                            kmax=kmax, rho=rho, verbose=verbose, \
+                            etar=etar, ratminus=ratminus, ratplus=ratplus)
         archargs = dict(archivepath=None, point='rsf', grad='rsf', filter='su', dataerr='su')
         sdargs = dict(Winv=Winv)
         lsargs = dict(lsmax=10, mured=0.5, muinc=1.8, gammared=0.1, gammainc=0.9)
         
-        Jx = vcalg.lsopt(m0, awi.mswi, SD=vcalg.SDwgrad, LS=vcalg.btls, descmax=descmax, desceps=desceps,\
+        Jx = vcalg.lsopt(m0, awi.mswi, SD=vcalg.SDwgrad, LS=vcalg.btls,\
+                             descmax=descmax, desceps=desceps,\
                              descverbose=descverbose, descout=descout,\
                              lsverbose=lsverbose, lsargs=lsargs, jetargs=mswiargs, ddargs=sdargs, archargs=archargs)
         m.copy(Jx.point())
