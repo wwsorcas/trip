8dfe0f3d313977d12ec406cebe770d3e1a8132be
William Symes <wwsorcas@gmail.com>
2024-03-15 09:07:04 -0700

    SPD smoother

--- repro/papers/awinum/mswi/fwi.py
+++ repro/papers/awinum/mswi/fwi.py
@@ -11,51 +11,12 @@ import segyvc
 import rsfvc
 import asg
 import awi
-import tmp
-
-class smoother(vcl.LinearOperator):
-
-    def __init__(self, dom, rect1=10, rect2=10, repeat=2):
-        RSFROOT = os.getenv('RSFROOT')
-        smoother = os.path.join(RSFROOT,'bin/sfsmooth')
-        self.dom = dom
-        if not isinstance(self.dom,rsfvc.Space):
-            raise Exception('grid smoother: provided domain not rsfvc.Space')
-        self.cmd = smoother + ' rect1=' + str(rect1) + ' rect2=' + str(rect2) + ' repeat=' +str(repeat)
-        
-    def getDomain(self):
-        return self.dom
-
-    def getRange(self):
-        return self.dom
-
-    def applyFwd(self,x,y):
-        try:
-            ret = os.system(self.cmd + ' < ' + x.data + ' > ' + y.data)
-            if ret != 0:
-                raise Exception('command failed: ' + self.cmd)
-        except Exception as ex:
-            print(ex)
-            raise Exception('called from smoother.applyFwd')
-
-    def applyAdj(self,x,y):
-        try:
-            ret = os.system(self.cmd + ' < ' + x.data + ' > ' + y.data)
-            if ret != 0:
-                raise Exception('command failed: ' + self.cmd)
-        except Exception as ex:
-            print(ex)
-            raise Exception('called from smoother.applyAdj')
-
-    def myNameIs():
-        print('2D Grid Smoother using sfsmooth')
-        print('rect1=10 rect2=10 repeat=2')
 
 def fwiopt(bulkmodout, bulkmodin, datain, buoyancy, source, 
                 order=2, sampord=1, nsnaps=20,\
                 cfl=0.5, cmin=1.0, cmax=3.0,dmin=0.8, dmax=3.0,\
                 nl1=250, nr1=250, nl2=250, nr2=250, pmlampl=1.0,
-                boundstest=True, noisy=False,
+                boundstest=True, noisy=False, partask=0,
                 rect1=10, rect2=10, repeat=2,
                 descmax=12, desceps=0.01, descverbose=1, descout=None,
                 lsmax=10, mured=0.5, muinc=1.8, gammared=0.1, gammainc=0.9, lsverbose=1
@@ -69,11 +30,11 @@ def fwiopt(bulkmodout, bulkmodin, datain, buoyancy, source,
         m0 = vcl.Vector(bulksp,bulkmodin)
         d = vcl.Vector(datasp,datain)
 
-        Winv = smoother(bulksp,rect1,rect2,repeat)
+        Winv = rsfvc.smoother(bulksp,rect1,rect2,repeat)
         
         F = asg.fsbop(bulksp, datasp, buoyancy, source, \
                     order, sampord, nsnaps, cfl, cmin, cmax, dmin, dmax,\
-                    nl1, nr1, nl2, nr2, pmlampl, boundstest, noisy \
+                    nl1, nr1, nl2, nr2, pmlampl, boundstest, noisy, partask \
                     )
 
         fwiargs = dict(F=F, b=d)
