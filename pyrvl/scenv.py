c3a86164a8afae3bb5be4ad8fa43bc93d42e7fd1
William Symes <wwsorcas@gmail.com>
2024-04-04 15:54:40 -0700

    updates

--- pyrvl/scenv.py
+++ pyrvl/scenv.py
@@ -4,6 +4,9 @@ APPS = os.path.join(HOME,'Library')
 CWPROOT = os.path.join(APPS,'CWP/44r16')
 RSFROOT = os.path.join(APPS,'madagascar/rsfroot')
 TRIP = os.path.join(HOME,'work/trip')
+MPIROOT = '/usr/local'
 os.environ['CWPROOT'] = CWPROOT
 os.environ['RSFROOT'] = RSFROOT
 os.environ['TRIP'] = TRIP
+os.environ['MPIROOT'] = MPIROOT
+
