17569916f94b3250ecde35450eda36e1655331c0
William Symes <wwsorcas@gmail.com>
2024-04-20 14:35:21 -0700

    fixes

--- pyrvl/mfexec.py
+++ pyrvl/mfexec.py
@@ -1,36 +1,36 @@
 #!/usr/bin/env python3
 
+import os
 import sys
+import scenv
 
 if __name__ == '__main__':   
     try:
 
-
-        #        print('check enough args')
+#        print('check enough args')
         if len(sys.argv) < 2:
             raise Exception('must be called with at least one argument ' + \
                                 '= name of function')
+                                
+#        print('sys.argv[1]')
+#        print(sys.argv[1])
 
         arga = sys.argv[1].split('.')
 #        print(arga)
         if len(arga) < 2:
             raise Exception('first arg must have form mod.fcn')
 
-#        print('arga[0] = ' + arga[0])
-
+#        print('import ' + arga[0])
         exec('import ' + arga[0])
 
-#        argl = ', '.join(sys.argv[2:])
+        argl = ', '.join(sys.argv[2:])
 
-#        cmd = sys.argv[1] + '(' + argl + ')'
+        cmd = sys.argv[1] + '(' + argl + ')'
             
 #        print('function call:')
 #        print(cmd)
 
-        print('sys.argv[1]')
-        print(sys.argv[1])
-
-        exec(str(sys.argv[1]))
+        exec(cmd)
         
     except Exception as ex:
         print(ex)
